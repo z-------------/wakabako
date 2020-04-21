6b26b176b0abd837403d0cdd867ebc559a2adb78
Zack Guard <zacharyguard@gmail.com>
2020-04-21 18:24:49 +0800

    Update code style

--- wakabako.rb
+++ wakabako.rb
@@ -11,59 +11,59 @@ require_relative 'helpers.rb'
 BASE = 'https://wakatime.com/api/v1'
 
 BAR_SCHEMES = [
-    ['░', '▏', '▎', '▍', '▌', '▋', '▊', '▉', '█'],
-    [' ', '▏', '▎', '▍', '▌', '▋', '▊', '▉', '█'],
+  ['░', '▏', '▎', '▍', '▌', '▋', '▊', '▉', '█'],
+  [' ', '▏', '▎', '▍', '▌', '▋', '▊', '▉', '█'],
 ]
 
 ### parse options ###
 
 opts = {
-    dry: false,
-    format: :long,
-    fractional: false,
-    help: false,
-    'include-percent': false,
-    'relative-bars': false,
-    scheme: 0,
-    width: 21,
+  dry: false,
+  format: :long,
+  fractional: false,
+  help: false,
+  'include-percent': false,
+  'relative-bars': false,
+  scheme: 0,
+  width: 21,
 }
 
 ARGV.each do |arg|
-    p = parse_opt arg
-    next if not p
+  p = parse_opt arg
+  next if not p
 
-    key = p[0].to_sym
-    val_str = p[1]
+  key = p[0].to_sym
+  val_str = p[1]
 
-    if not opts.include? key
-        puts "Ignoring unknown option '#{key}'."
-        next
-    end
+  if not opts.include? key
+    puts "Ignoring unknown option '#{key}'."
+    next
+  end
 
-    if is_b opts[key]
-        if not val_str
-            val = true
-        else
-            val = val_str.to_b
-        end
+  if is_b opts[key]
+    if not val_str
+      val = true
     else
-        if not val_str
-            seppuku "Invalid value given for option '#{key}'"
-        end
-        if opts[key].class.equal? Integer
-            val = val_str.to_i
-        else
-            val = val_str.to_sym
-        end
+      val = val_str.to_b
     end
+  else
+    if not val_str
+      seppuku "Invalid value given for option '#{key}'"
+    end
+    if opts[key].class.equal? Integer
+      val = val_str.to_i
+    else
+      val = val_str.to_sym
+    end
+  end
 
-    opts[key] = val
+  opts[key] = val
 end
 
 ### main ###
 
 if opts[:help]
-    puts <<-END
+  puts <<-END
 Options:
   --help            Print this help and exit.                       [boolean] [default=false]
   --dry             Print to stdout instead of uploading a gist.    [boolean] [default=false]
@@ -77,17 +77,17 @@ Options:
                     SCHEME can be 0 or 1.                           [integer] [default=0]
   --width=WIDTH     The number of characters to use for each bar.   [integer] [default=21]
     END
-    exit
+  exit
 end
 
 begin
-    config = read_config("#{__dir__}/config.txt")
+  config = read_config("#{__dir__}/config.txt")
 rescue Errno::ENOENT
-    begin
-        config = read_config("#{__dir__}/config.toml")
-    rescue Errno::ENOENT
-        seppuku 'No config file found. Exiting.'
-    end
+  begin
+    config = read_config("#{__dir__}/config.toml")
+  rescue Errno::ENOENT
+    seppuku 'No config file found. Exiting.'
+  end
 end
 auth_key_hashed = Base64.encode64(config['auth_key']).chomp
 
@@ -96,48 +96,48 @@ req = Net::HTTP::Get.new(uri)
 req['Authorization'] = "Basic #{auth_key_hashed}"
 
 res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => true) do |http|
-    http.request(req)
+  http.request(req)
 end
 
 table = []
 JSON.parse(res.body)['data']['languages'][0...5].each_with_index do |language, i|
-    row = []
+  row = []
 
-    hours = language['hours']
-    mins = language['minutes']
-    percent = language['percent']
-    time_str = format_duration(hours, mins, opts[:format])
+  hours = language['hours']
+  mins = language['minutes']
+  percent = language['percent']
+  time_str = format_duration(hours, mins, opts[:format])
 
-    perc = percent / 100
-    if opts[:'relative-bars']
-        if i == 0
-            $percent_max = percent
-            perc = 1.0
-        else
-            perc = percent / $percent_max
-        end
+  perc = percent / 100
+  if opts[:'relative-bars']
+    if i == 0
+      $percent_max = percent
+      perc = 1.0
+    else
+      perc = percent / $percent_max
     end
-    bar = make_bar(perc, opts[:width], opts[:scheme], opts[:fractional])
+  end
+  bar = make_bar(perc, opts[:width], opts[:scheme], opts[:fractional])
 
-    row << language['name'] << time_str << bar
-    if opts[:'include-percent']
-        row << "#{percent.round.to_s.rjust 2}%"
-    end
-    table << row
+  row << language['name'] << time_str << bar
+  if opts[:'include-percent']
+    row << "#{percent.round.to_s.rjust 2}%"
+  end
+  table << row
 end
 
 lines = format_cols(table, [[0, 2], [0, 1], [0, 0], [1, 0]])
 if lines.empty?
-    lines << 'Nothing here.'
+  lines << 'Nothing here.'
 end
 
 formatted = lines.join("\n") + "\n"
 
 if opts[:dry]
-    puts formatted
+  puts formatted
 else
-    Gist.gist(formatted, {
-        update: config['gist_id'],
-        filename: config['title']
-    })
+  Gist.gist(formatted, {
+    update: config['gist_id'],
+    filename: config['title'],
+  })
 end
